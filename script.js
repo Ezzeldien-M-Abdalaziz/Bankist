3223fbb7ca556e868abadf51819d50e58a2785fb
Ezzeldien <ezzeldien.m.abdalziz@gmail.com>
2023-12-16 15:10:23 +0300

    editing the openModel Event

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventdefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
