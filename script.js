cfac2cece279fac1fdc99c1c7755a668a59de5e9
Ezzeldien <ezzeldien.m.abdalziz@gmail.com>
2023-12-26 18:38:22 +0300

    lazy load images

--- script.js
+++ script.js
@@ -168,3 +168,29 @@ const headerObserver = new IntersectionObserver(stickyNav,
       sectionObserver.observe(section);
       section.classList.add('section--hidden');
     });
+
+    //loading lazy images 
+
+    const imgTargets = document.querySelectorAll('img[data-src]');
+
+    const loadImg = function(entries,observer){
+      const [entry] = entries;
+      if(!entry.isIntersecting) return;
+
+      //replace src with data-src
+      entry.target.src = entry.target.dataset.src;
+
+      entry.target.addEventListener('load',function(){
+        entry.target.classList.remove('lazy-img');
+      });
+        observer.unobserve(entry.target);
+    };
+
+    const imgObserver = new IntersectionObserver(loadImg,
+      {
+        root:null,
+        threshold:0,
+        rootMargin:'200px'
+    });
+
+    imgTargets.forEach(img=>imgObserver.observe(img));
